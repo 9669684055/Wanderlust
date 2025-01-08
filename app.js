41417f4b1eef466a5d7878879f14323fcea956a7
Vivek yadav <vy7052540@gmail.com>
2025-01-08 14:06:53 +0530

    Initial commit

--- app.js
+++ app.js
@@ -45,10 +45,11 @@ async function main () {
 
 app.set("view engine", "ejs");
 app.set("views" , path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({extended :true}));
 app.use(methodOverride("_method"));
 app.engine('ejs' , ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+
 
 const store = MongoStore.create({
     mongoUrl:dbUrl,
